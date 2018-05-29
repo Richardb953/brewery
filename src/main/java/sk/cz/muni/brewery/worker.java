a447aad372d4e04e28b99d2820a6394f3fe05f82
manager1 <>
2018-05-29 14:47:06 +0200

    {/src/main/java/sk/cz/muni/brewery/worker.java}

--- src/main/java/sk/cz/muni/brewery/worker.java
+++ src/main/java/sk/cz/muni/brewery/worker.java
@@ -7,29 +7,80 @@ package sk.cz.muni.brewery;
 @javax.persistence.Entity
 public class worker implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "WORKER_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "WORKER_ID_GENERATOR", sequenceName = "WORKER_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "WORKER_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "WORKER_ID_GENERATOR", sequenceName = "WORKER_ID_SEQ")
+	private java.lang.Long id;
 
-    public worker() {
-    }
-    
-    public worker(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Company mail")
+	private java.lang.String workmail;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	@javax.persistence.ManyToOne(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
+	@org.kie.api.definition.type.Label(value = "Worker CV information")
+	private sk.cz.muni.brewery.cv cv;
 
+	@javax.persistence.ManyToOne(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
+	@org.kie.api.definition.type.Label(value = "Worker position")
+	private sk.cz.muni.brewery.position_offer position;
 
+	@javax.persistence.ManyToOne(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
+	@org.kie.api.definition.type.Label(value = "Worker position type")
+	private sk.cz.muni.brewery.positionType positionType;
 
+	public worker() {
+	}
+
+	public java.lang.Long getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public java.lang.String getWorkmail() {
+		return this.workmail;
+	}
+
+	public void setWorkmail(java.lang.String workmail) {
+		this.workmail = workmail;
+	}
+
+	public sk.cz.muni.brewery.cv getCv() {
+		return this.cv;
+	}
+
+	public void setCv(sk.cz.muni.brewery.cv cv) {
+		this.cv = cv;
+	}
+
+	public sk.cz.muni.brewery.position_offer getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(sk.cz.muni.brewery.position_offer position) {
+		this.position = position;
+	}
+
+	public sk.cz.muni.brewery.positionType getPositionType() {
+		return this.positionType;
+	}
+
+	public void setPositionType(sk.cz.muni.brewery.positionType positionType) {
+		this.positionType = positionType;
+	}
+
+	public worker(java.lang.Long id, java.lang.String workmail,
+			sk.cz.muni.brewery.cv cv,
+			sk.cz.muni.brewery.position_offer position,
+			sk.cz.muni.brewery.positionType positionType) {
+		this.id = id;
+		this.workmail = workmail;
+		this.cv = cv;
+		this.position = position;
+		this.positionType = positionType;
+	}
 
 }
