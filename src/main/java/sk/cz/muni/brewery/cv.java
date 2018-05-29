985087138c5dd4e4a58a307ffb20f78dad68142b
manager1 <>
2018-05-29 14:43:45 +0200

    {/src/main/java/sk/cz/muni/brewery/cv.java}

--- src/main/java/sk/cz/muni/brewery/cv.java
+++ src/main/java/sk/cz/muni/brewery/cv.java
@@ -7,29 +7,127 @@ package sk.cz.muni.brewery;
 @javax.persistence.Entity
 public class cv implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "CV_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "CV_ID_GENERATOR", sequenceName = "CV_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "CV_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "CV_ID_GENERATOR", sequenceName = "CV_ID_SEQ")
+	private java.lang.Long id;
 
-    public cv() {
-    }
-    
-    public cv(java.lang.Long id) {
-        this.id = id;
-    }
+	@javax.persistence.ManyToOne(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
+	@org.kie.api.definition.type.Label(value = "Work history")
+	private sk.cz.muni.brewery.positionConditions conditions;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
 
+	@org.kie.api.definition.type.Label(value = "Surname")
+	private java.lang.String surname;
 
+	@org.kie.api.definition.type.Label(value = "Telephone number")
+	private java.lang.String tel;
 
+	@org.kie.api.definition.type.Label(value = "Mail adress")
+	private java.lang.String mail;
+
+	@org.kie.api.definition.type.Label(value = "Age")
+	private java.lang.Integer age;
+
+	@org.kie.api.definition.type.Label(value = "Work history")
+	private java.lang.String previousJob;
+
+	@org.kie.api.definition.type.Label(value = "Why do you want to work for us")
+	private java.lang.String reason;
+
+	public cv() {
+	}
+
+	public java.lang.Long getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
+
+	public sk.cz.muni.brewery.positionConditions getConditions() {
+		return this.conditions;
+	}
+
+	public void setConditions(sk.cz.muni.brewery.positionConditions conditions) {
+		this.conditions = conditions;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(java.lang.String surname) {
+		this.surname = surname;
+	}
+
+	public java.lang.String getTel() {
+		return this.tel;
+	}
+
+	public void setTel(java.lang.String tel) {
+		this.tel = tel;
+	}
+
+	public java.lang.String getMail() {
+		return this.mail;
+	}
+
+	public void setMail(java.lang.String mail) {
+		this.mail = mail;
+	}
+
+	public java.lang.Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(java.lang.Integer age) {
+		this.age = age;
+	}
+
+	public java.lang.String getPreviousJob() {
+		return this.previousJob;
+	}
+
+	public void setPreviousJob(java.lang.String previousJob) {
+		this.previousJob = previousJob;
+	}
+
+	public java.lang.String getReason() {
+		return this.reason;
+	}
+
+	public void setReason(java.lang.String reason) {
+		this.reason = reason;
+	}
+
+	public cv(java.lang.Long id,
+			sk.cz.muni.brewery.positionConditions conditions,
+			java.lang.String name, java.lang.String surname,
+			java.lang.String tel, java.lang.String mail, java.lang.Integer age,
+			java.lang.String previousJob, java.lang.String reason) {
+		this.id = id;
+		this.conditions = conditions;
+		this.name = name;
+		this.surname = surname;
+		this.tel = tel;
+		this.mail = mail;
+		this.age = age;
+		this.previousJob = previousJob;
+		this.reason = reason;
+	}
 
 }
