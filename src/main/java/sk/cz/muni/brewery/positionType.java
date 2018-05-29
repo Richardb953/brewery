0c044129472448fca046b678f32d11f7ff06acf3
manager1 <>
2018-05-29 14:38:46 +0200

    {/src/main/java/sk/cz/muni/brewery/positionType.java}

--- src/main/java/sk/cz/muni/brewery/positionType.java
+++ src/main/java/sk/cz/muni/brewery/positionType.java
@@ -7,29 +7,38 @@ package sk.cz.muni.brewery;
 @javax.persistence.Entity
 public class positionType implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "POSITIONTYPE_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "POSITIONTYPE_ID_GENERATOR", sequenceName = "POSITIONTYPE_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "POSITIONTYPE_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "POSITIONTYPE_ID_GENERATOR", sequenceName = "POSITIONTYPE_ID_SEQ")
+	private java.lang.Long id;
 
-    public positionType() {
-    }
-    
-    public positionType(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Position type")
+	private java.lang.String name;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	public positionType() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public positionType(java.lang.Long id, java.lang.String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 }
